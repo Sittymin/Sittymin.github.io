09f812ad92ce24a484a3e1aa1980db8412284d47
Sittymin <2890108976@qq.com>
2023-12-20 12:12:09 +0800

    update

---
+++ scripts/other.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('.register');
+
+button.addEventListener('click', () => {
+    fetch("http://172.30.179.248:10001/prod-api/api/register", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            userName: "test",
+            password: "",
+            phonenumber: "1",
+            sex: "0"
+        })
+    })
+})
+.then(response => response.json())
+.then(data => console.log(data))
